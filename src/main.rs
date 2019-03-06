1b7c7b22eb52e49d29d7e87e44226f8be6208b46
Mikhail Pokhikhilov <thedilletante@gmail.com>
2019-03-06 23:02:56 +0300

    Made Tape just HashMap for random-access
    Double-linked list is not Rusty way to solve problems.
    So, the ownership model was made easier.
    Tape is a hash map with i64 as an key (represents Head)
    
    Turing related code moved to turing.rs mod

--- src/main.rs
+++ src/main.rs
@@ -1,151 +1,14 @@
-use std::rc::Rc;
-use std::cell::RefCell;
 use std::fmt::Debug;
 use std::collections::HashMap;
-use std::hash::Hash;
 
-/// Turing machine implementation assumes existence
-/// of infinite tape with cells that represents memory.
-///
-/// My realization consist of two concepts:
-///  - a double linked list, where node represents memory unit in the tape;
-///  - a cursor which represents a head under the tape.
-/// To support moving the cursor to any distance to any direction
-/// values of the list is optional to represent empty element.
-/// It means the moves of the cursor is able to modify underlaying list.
-///
-/// Other implementations are also possible:
-///  - two containers (vector/list) that are halves of tape
-///    directed into different ways (forward/backward);
-///  - whenever you can develop.
+mod turing;
 
-type CellLink <T> = Rc<RefCell<Cell<T>>>;
 
-struct Cell <T> {
-  value: Option<T>,
-  left: Option<CellLink<T>>,
-  right: Option<CellLink<T>>
-}
-
-impl <T> Cell <T> {
-
-  fn single(value: Option<T>) -> CellLink<T> {
-    Cell::link(value, None, None)
-  }
-
-  fn left_link(value: Option<T>, right: CellLink<T>) -> CellLink<T> {
-    let link = Cell::link(value, None, Some(right.clone()));
-    right.borrow_mut().left = Some(link.clone());
-    link
-  }
-
-  fn right_link(value: Option<T>, left: CellLink<T>) -> CellLink<T> {
-    let link = Cell::link(value, Some(left.clone()), None);
-    left.borrow_mut().right = Some(link.clone());
-    link
-  }
-
-  fn link(
-    value: Option<T>,
-    left: Option<CellLink<T>>,
-    right: Option<CellLink<T>>
-  ) -> CellLink<T> {
-    Rc::new(
-      RefCell::new(
-        Cell {
-          value,
-          left,
-          right
-        }
-      )
-    )
-  }
-}
-
-/// Cursor moves under tape that it not owned by it.
-/// So cloning cursor means making the snapshot
-/// of the position of the head under the tape.
-#[derive(Clone)]
-struct Cursor <T> {
-  current: CellLink<T>
-}
-
-/// the value of the cell is returned by value in lookup()
-/// thus type must support copying
-impl <T> Cursor <T>
-where T: Copy {
-
-  fn on_new_empty_tape() -> Cursor<T> {
-    Cursor {
-      current: Cell::single(None)
-    }
-  }
-
-  fn lookup(&self) -> Option<T> {
-    self.current.borrow().value
-  }
-
-  fn set(&mut self, value: Option<T>) {
-    self.current.borrow_mut().value = value;
-  }
-
-  /// Cell::right_link()/Cell::left_link() implementation borrow mutable
-  /// reference of provided link to set itself as appropriate link.
-  /// It means we can't use match expression to match the link because it will
-  /// be borrowed inside the whole scope of match expression.
-
-  fn move_right(&mut self) -> Option<T> {
-    let right_cell = if self.current.borrow().right.is_none() {
-      Cell::right_link(None, self.current.clone())
-    } else {
-      self.current.borrow().right.as_ref().unwrap().clone()
-    };
-
-    let ret = self.lookup();
-    self.current = right_cell;
-    ret
-  }
-
-  fn move_left(&mut self) -> Option<T> {
-    let left_cell = if self.current.borrow().left.is_none() {
-      Cell::left_link(None, self.current.clone())
-    } else {
-      self.current.borrow().left.as_ref().unwrap().clone()
-    };
-
-    let ret = self.lookup();
-    self.current = left_cell;
-    ret
-  }
-}
-
-/// Be careful. The iterators for cursor type are infinite.
-/// They are just making new empty nodes as making progress at any way.
-impl <T> Iterator for Cursor <T>
-where T: Copy {
-  type Item = Cursor<T>;
-
-  fn next(&mut self) -> Option<Self::Item> {
-    let ret = Some(self.clone());
-    self.move_right();
-    ret
-  }
-}
-
-impl <T> DoubleEndedIterator for Cursor <T>
-where T: Copy {
-  fn next_back(&mut self) -> Option<Self::Item> {
-    let ret = Some(self.clone());
-    self.move_left();
-    ret
-  }
-}
-
-fn print_jointed<T> (cursor_iterator: impl Iterator<Item=Cursor<T>>)
+fn print_jointed<T> (iter: impl Iterator<Item=Option<T>>)
 where T: Copy + Debug + ToString {
   println!("{}",
-           cursor_iterator
-             .map(|a| match a.lookup() {
+           iter
+             .map(|a| match a {
                None => "_".to_string(),
                Some(a) => a.to_string()
              })
@@ -155,86 +18,16 @@ where T: Copy + Debug + ToString {
 }
 
 
-#[derive(Debug)]
-enum Move {
-  Stay,
-  Left,
-  Right
-}
-
 #[derive(Hash, Eq, PartialEq, Debug)]
 struct Observed <Symbol, State> {
   symbol: Option<Symbol>,
   state: State
 }
 
-#[derive(Debug)]
-enum Transition <State> {
-  State(State),
-  Halt
-}
-
-#[derive(Debug)]
-struct Action <Symbol, State> {
-  set: Option<Symbol>,
-  movement: Move,
-  transition: Transition<State>
-}
-
 type Program <Symbol, State> = HashMap<
   Observed<Symbol, State>,
-  Action<Symbol, State>
+  turing::Action<Symbol, State>
 >;
-
-struct IdleVirtualMachine <State> {
-  state: State
-}
-
-enum VirtualMachine <State> {
-  Idle(IdleVirtualMachine<State>),
-  Done
-}
-
-impl <State> IdleVirtualMachine <State>
-where State: Copy {
-
-  fn new(state: State) -> IdleVirtualMachine <State> {
-    IdleVirtualMachine {
-      state
-    }
-  }
-
-  fn apply<Symbol>(&self, cursor: &mut Cursor<Symbol>, action: &Action<Symbol, State>) -> VirtualMachine<State>
-  where Symbol: Copy,
-        Observed<Symbol, State>: Hash + Eq {
-
-    cursor.set(action.set);
-    match action.movement {
-      Move::Left => {
-        cursor.move_left();
-      },
-      Move::Right => {
-        cursor.move_right();
-      },
-      _ => ()
-    };
-
-    match action.transition {
-      Transition::State(new_state) => VirtualMachine::Idle(IdleVirtualMachine::new(new_state)),
-      Transition::Halt => VirtualMachine::Done
-    }
-  }
-}
-
-impl <State> VirtualMachine <State>
-where State: Copy {
-  fn new(initial: State) -> VirtualMachine <State> {
-    VirtualMachine::Idle(IdleVirtualMachine{
-      state: initial
-    })
-  }
-}
-
 
 fn build_simple_program() -> Program<char, char> {
   let mut program = HashMap::new();
@@ -242,28 +35,28 @@ fn build_simple_program() -> Program<char, char> {
   program.insert(Observed{
     symbol: Some('a'),
     state: 'a'
-  }, Action {
-    transition: Transition::State('b'),
+  }, turing::Action {
+    transition: turing::Transition::State('b'),
     set: Some('b'),
-    movement: Move::Left
+    movement: turing::Movement::Left
   });
 
   program.insert(Observed{
     symbol: Some('b'),
     state: 'b'
-  }, Action {
-    transition: Transition::State('b'),
+  }, turing::Action {
+    transition: turing::Transition::State('b'),
     set: None,
-    movement: Move::Right
+    movement: turing::Movement::Right
   });
 
   program.insert(Observed{
     symbol: None,
     state: 'b'
-  }, Action {
-    transition: Transition::Halt,
+  }, turing::Action {
+    transition: turing::Transition::Halt,
     set: Some('a'),
-    movement: Move::Stay
+    movement: turing::Movement::Stay
   });
 
   program
@@ -272,69 +65,29 @@ fn build_simple_program() -> Program<char, char> {
 fn simple_program() {
 
   let program = build_simple_program();
-  let mut cursor = Cursor::on_new_empty_tape();
+  let mut tape = turing::Tape::new();
+  let mut vm = turing::VirtualMachine::new('a');
 
-  cursor.set(Some('a'));
+  tape.set(0, Some('a'));
+
+  let head = 0;
   // print it from left to right
-  print_jointed(
-    cursor.clone()
-      .rev().nth(2).unwrap().take(5)
-  );
+  print_jointed(tape.iter_with(head - 2).take(5));
+  while let turing::VirtualMachine::Idle(state) = vm {
 
-  let mut machine = VirtualMachine::new('a');
-
-  while let VirtualMachine::Idle(idle) = machine {
-
-    let instruction = program.get(&Observed{
-      symbol: cursor.lookup(),
-      state: idle.state
+    let action = program.get(&Observed{
+      symbol: tape.value(head),
+      state
     }).unwrap();
 
-    machine = idle.apply(&mut cursor, instruction);
+    let (new_vm, head) = turing::execute(&vm, action, &mut tape, head);
+    vm = new_vm;
 
     // print it from left to right
-    print_jointed(
-      cursor.clone()
-        .rev().nth(2).unwrap().take(5)
-    );
+    print_jointed(tape.iter_with(head - 2).take(5));
   }
 }
 
-//#[warn(dead_code)]
-//fn cursor_test() {
-//  let cursor = Cursor::on_new_empty_tape();
-//
-//  // fill the range
-//  cursor.clone()
-//    .take(5)
-//    .enumerate()
-//    .for_each(|(i,mut e)| e.set(Some(i)));
-//
-//  // make an empty cell in the middle
-//  cursor.clone()
-//    .nth(2).unwrap()
-//    .set(None);
-//
-//  // print it from left to right
-//  print_jointed(
-//    cursor.clone()
-//      .take(5)
-//  );
-//
-//  // print from right to left
-//  print_jointed(
-//    cursor.clone()
-//      // starting with 4th element (or 5th if you start counting with 1)
-//      .nth(4).unwrap()
-//      // reverse sequence
-//      .rev()
-//      // and print only 5 elements
-//      .take(5)
-//  );
-//}
-
 fn main() {
-
-
   simple_program();
 }
---
+++ src/turing.rs
@@ -0,0 +1,171 @@
+use std::collections::HashMap;
+
+/// Turing machine abstraction requires existence of
+/// infinite tape with cells which represents memory.
+/// The user is able to walk through the tape in any
+/// direction using head by one step at once.
+/// Is it possible to look at the cell's content
+/// under the head or to change it.
+
+pub struct Tape <T> {
+  cells: HashMap<i64, T>
+}
+
+impl <T> Tape <T>
+where T: Copy {
+
+  pub fn new() -> Tape<T> {
+    Tape {
+      cells: HashMap::new()
+    }
+  }
+
+  pub fn value(&self, head: i64) -> Option<T> {
+    self.cells.get(&head).map(|x| *x)
+  }
+
+  pub fn set(&mut self, head: i64, value: Option<T>) -> Option<T> {
+    match value {
+      None => self.cells.remove(&head),
+      Some(v) => self.cells.insert(head, v)
+    }
+  }
+
+}
+
+pub enum Movement {
+  Left,
+  Right,
+  Stay
+}
+
+pub enum Transition <S> {
+  State(S),
+  Halt
+}
+
+pub struct Action <T, S> {
+  pub set: Option<T>,
+  pub movement: Movement,
+  pub transition: Transition<S>
+}
+
+pub enum VirtualMachine <S> {
+  Idle(S),
+  Done
+}
+
+impl <S> VirtualMachine <S>
+where S: Copy {
+
+  pub fn new(state: S) -> VirtualMachine<S> {
+    VirtualMachine::Idle(state)
+  }
+
+}
+
+pub fn execute <T, S> (vm: &VirtualMachine<S>, action: &Action<T, S>, tape: &mut Tape<T>, head: i64)
+  -> (VirtualMachine<S>, i64)
+where T: Copy,
+      S: Copy {
+  match vm {
+    VirtualMachine::Done => (VirtualMachine::Done, head),
+    VirtualMachine::Idle(_) => {
+      tape.set(head, action.set);
+
+      let new_head = match action.movement {
+        Movement::Left => head + 1,
+        Movement::Right => head - 1,
+        Movement::Stay => head
+      };
+
+      let vm_state = match action.transition {
+        Transition::Halt => VirtualMachine::Done,
+        Transition::State(state) => VirtualMachine::Idle(state)
+      };
+
+      (vm_state, new_head)
+    }
+  }
+}
+
+/// The iterator trait is implemented for Tape just for convenience.
+pub struct TapeIter <'a, T> {
+  tape: &'a Tape<T>,
+  head: i64
+}
+
+impl <T> Tape <T>
+where T: Copy {
+
+  #[allow(dead_code)]
+  pub fn iter_with(&self, head: i64) -> TapeIter<T> {
+    TapeIter {
+      tape: self,
+      head
+    }
+  }
+
+  #[allow(dead_code)]
+  pub fn iter(&self) -> TapeIter<T> {
+    self.iter_with(0)
+  }
+}
+
+impl <'a, T> Iterator for TapeIter <'a, T>
+where T: Copy {
+  type Item = Option<T>;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    let ret = self.tape.value(self.head);
+    self.head += 1;
+    Some(ret)
+  }
+}
+
+impl <'a, T> DoubleEndedIterator for TapeIter <'a, T>
+where T: Copy {
+  fn next_back(&mut self) -> Option<Self::Item> {
+    let ret = self.tape.value(self.head);
+    self.head -= 1;
+    Some(ret)
+  }
+}
+
+#[test]
+fn tape_test() {
+  let mut tape = Tape::new();
+
+  for i in -1000..1000 {
+    assert_eq!(None, tape.value(i));
+  }
+
+  assert_eq!(None, tape.set(0, Some('a')));
+  assert_eq!(Some('a'), tape.value(0));
+
+  assert_eq!(Some('a'), tape.set(0, None));
+  assert_eq!(None, tape.value(0));
+}
+
+#[test]
+fn iter_test() {
+  let mut tape = Tape::new();
+  (0..5).for_each(|x| assert_eq!(None, tape.set(x, Some(x))));
+
+  let mut forward_iter = tape.iter();
+  assert_eq!(Some(0), forward_iter.next().unwrap());
+  assert_eq!(Some(1), forward_iter.next().unwrap());
+  assert_eq!(Some(2), forward_iter.next().unwrap());
+  assert_eq!(Some(3), forward_iter.next().unwrap());
+  assert_eq!(Some(4), forward_iter.next().unwrap());
+  assert_eq!(None, forward_iter.next().unwrap());
+
+  let mut backward_iter = tape.iter_with(5).rev();
+  assert_eq!(None, backward_iter.next().unwrap());
+  assert_eq!(Some(4), backward_iter.next().unwrap());
+  assert_eq!(Some(3), backward_iter.next().unwrap());
+  assert_eq!(Some(2), backward_iter.next().unwrap());
+  assert_eq!(Some(1), backward_iter.next().unwrap());
+  assert_eq!(Some(0), backward_iter.next().unwrap());
+  assert_eq!(None, backward_iter.next().unwrap());
+}
